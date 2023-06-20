cc0175ea290fd974173204e718ae646ba600a068
Theam-Dev <phearakroun@gmail.com>
2023-06-20 18:42:33 +0700

    react uuid

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { v4 as uuidv4 } from 'uuid';
+import React, { useState } from 'react';
 
 function App() {
+  const [ids, setIds] = useState(uuidv4());
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" width={200}/>
+        <div>UUID: {ids}</div><br/>
+        <div>UUID: {ids}</div><br/>
+        <div>UUID: {ids}</div><br/>
       </header>
     </div>
   );
